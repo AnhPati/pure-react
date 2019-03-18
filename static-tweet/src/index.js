95d6881f647476178ee3aa09eaa00dc990ca1070
Anh Pati <romain-felicianne@hotmail.fr>
2019-03-18 05:31:11 +0100

    Initialisation du fichier index.js

--- static-tweet/src/index.js
+++ static-tweet/src/index.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Tweet() {
+    <div className="tweet">
+        Tweet
+    </div>
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Tweet/>, document.getElementById("root"));
