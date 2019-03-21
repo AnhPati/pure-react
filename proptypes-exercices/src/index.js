1c5f58644aa8742bba68f7aa0b39d45a2361b851
Anh Pati <romain-felicianne@hotmail.fr>
2019-03-21 13:37:25 +0100

    Création du container commun aux exercices.

---
+++ proptypes-exercices/src/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Exercices() {
+    return (
+        <div>
+            <h1></h1>
+            <ul>
+                <li>
+                    <h2></h2>
+                </li>
+                <li>
+                    <h2></h2>
+                </li>
+                <li>
+                    <h2></h2>
+                </li>
+                <li>
+                    <h2></h2>
+                </li>
+                <li>
+                    <h2></h2>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+ReactDOM.render(<Exercices/>, document.getElement("root"))
