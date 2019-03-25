ba8dbb933f52f8210995ebe9bf94174850917822
Anh Pati <romain-felicianne@hotmail.fr>
2019-03-26 00:50:44 +0100

    Création du composant ErrorBox. Ajout des contenus au composant ErrorBox. Ajout des styles au composant ErrorBox.

--- children-exercices/src/index.js
+++ children-exercices/src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function ChildrenExercices() {
+    return (
+        <div class="mainContainer">
+            <ErrorBox>
+                The world is ending.
+            </ErrorBox>
+        </div>
+    );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function ErrorBox({children}) {
+    return (
+        <div className="alert alert-danger warningBox" role="alert">
+            <div className="iconBox">
+                <i class="fas fa-exclamation-triangle"></i>
+            </div>
+            <div className="textBox">
+                <p>
+                    {children}
+                </p>
+            </div>
+        </div>
+    )
+}
+
+ReactDOM.render(<ChildrenExercices/>, document.getElementById("root"));
