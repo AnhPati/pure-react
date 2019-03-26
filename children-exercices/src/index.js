92a38fd4a276add404da9c3b256ff5438b8f0e4d
Anh Pati <romain-felicianne@hotmail.fr>
2019-03-26 18:55:39 +0100

    Création de l'exercice 2-a Ajout du contenu de l'exercice 2-a Ajout des styles de l'exercice 2 Création de l'exercice 2-b Ajout du contenu de l'exercice 2-b

--- children-exercices/src/index.js
+++ children-exercices/src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css"
+import PropTypes from 'prop-types';
+import "./index.css";
 
 function ChildrenExercices() {
     return (
-        <div class="mainContainer">
+        <div className="mainContainer">
+            <h2 className="c-exerciceTitle">Exercice 1 :</h2>
             <ErrorBox>
                 The world is ending.
             </ErrorBox>
+            <h2 className="c-exerciceTitle">Exercices 2 :</h2>
+            <ChildrenArray/>
         </div>
     );
 };
@@ -16,7 +20,7 @@ function ErrorBox({children}) {
     return (
         <div className="alert alert-danger warningBox" role="alert">
             <div className="iconBox">
-                <i class="fas fa-exclamation-triangle"></i>
+                <i className="fas fa-exclamation-triangle"></i>
             </div>
             <div className="textBox">
                 <p>
@@ -24,7 +28,88 @@ function ErrorBox({children}) {
                 </p>
             </div>
         </div>
-    )
+    );
+};
+
+ErrorBox.propTypes = {
+    children: PropTypes.node.isRequired
+};
+
+function ChildrenArray() {
+    return (
+        <div>
+            <FirstChildOnly>
+                <span>
+                    🐦
+                </span>
+                <span>
+                    🐧
+                </span>
+                <span>
+                    🦅
+                </span>
+                <span>
+                    🦆
+                </span>
+                <span>
+                    🦉
+                </span>
+            </FirstChildOnly>
+            <LastChildOnly>
+                <span>
+                    🐟
+                </span>
+                <span>
+                    🐠
+                </span>
+                <span>
+                    🐡
+                </span>
+                <span>
+                    🦈
+                </span>
+            </LastChildOnly>
+        </div>
+    );
+};
+
+function FirstChildOnly({children}) {
+    let firstChild = React.Children.toArray(children)[0];
+    return (
+        <div className="childBox u-flexBox centered column u-marginBottom-lg">
+            <p className="paragraph-md u-marginBottom-sm u-flexBox">
+                <span className="c-circle u-marginRight-xs">a</span>
+                <span className="decored">Escoge tu pajaro :</span>
+            </p>
+            <p>
+                {firstChild}
+            </p>
+        </div>
+    );
+};
+
+FirstChildOnly.propTypes = {
+    children: PropTypes.node.isRequired
+};
+
+function LastChildOnly({children}) {
+    let allChildren = React.Children.toArray(children);
+    let lastChild = allChildren.length - 1;
+    return (
+        <div className="childBox u-flexBox centered column u-marginBottom-lg">
+            <p className="paragraph-md u-marginBottom-sm u-flexBox">
+                <span className="c-circle u-marginRight-xs">b</span>
+                <span className="decored">Escoge tu pez :</span>
+            </p>
+            <p>
+                {lastChild}
+            </p>
+        </div>
+    );
+};
+
+LastChildOnly.propTypes = {
+    children: PropTypes.node.isRequired
 }
 
 ReactDOM.render(<ChildrenExercices/>, document.getElementById("root"));
