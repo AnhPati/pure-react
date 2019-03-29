0b0fae89d475d0b1d4a18a5e713aac05720381a9
Anh Pati <romain-felicianne@hotmail.fr>
2019-03-30 00:01:08 +0100

    Correction de l'exercice 2 : Exportation de FileName et importation dans file-list-item.js. Exportation des composants FileIcon et CommitMessage et importation dans file-name.js.

--- github-file-list/src/file-list-item.js
+++ github-file-list/src/file-list-item.js
@@ -1,8 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import FileIcon from "./file-icon";
-import CommitMessage from "./commit-message";
-import Time from "./time";
+import FileName from "./file-name";
 import "./index.css";
 
 const FileListItem = ({file}) => (
@@ -10,33 +8,5 @@ const FileListItem = ({file}) => (
         <FileName file={file}/>
     </tr>
 );
-
-FileListItem.propTypes = {
-    file: PropTypes.object.isRequired
-}
-
-function FileName({file}) {
-    return (
-    <React.Fragment>
-        <td className="file-icon">
-            <FileIcon type={file.type}/>
-        </td>
-        <td className="file-name">
-            {file.name}
-        </td>
-        <td className="commit-message">
-            <CommitMessage message={file.lastestCommit.message}/>
-        </td>
-        <td className="age">
-            <Time time={file.updated_at}/>
-        </td>
-    </React.Fragment>
-)};
-
-FileName.propTypes = {
-    file: PropTypes.shape({
-        name: PropTypes.string.isRequired
-    })
-};
 
 export default FileListItem;
---
+++ github-file-list/src/file-name.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+import FileIcon from "./file-icon";
+import CommitMessage from "./commit-message";
+import Time from "./time";
+import "./index.css";
+
+function FileName({file}) {
+    return (
+    <React.Fragment>
+        <td className="file-icon">
+            <FileIcon type={file.type}/>
+        </td>
+        <td className="file-name">
+            {file.name}
+        </td>
+        <td className="commit-message">
+            <CommitMessage message={file.lastestCommit.message}/>
+        </td>
+        <td className="age">
+            <Time time={file.updated_at}/>
+        </td>
+    </React.Fragment>
+)};
+
+FileName.propTypes = {
+    file: PropTypes.shape({
+        name: PropTypes.string.isRequired
+    })
+};
+
+export default FileName;
