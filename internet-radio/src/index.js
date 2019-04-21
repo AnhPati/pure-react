e45a99ba823f98f7f99bbcb657f115b782c49e30
Anh Pati <romain-felicianne@hotmail.fr>
2019-04-21 10:58:30 +0200

    Ajout de données à la variable radioDatas. Ajout de classes aux composants RadioContainer et RadioButton. Modification de la condition ternaire qui renseigne une classe selon l'id de l'élément.

--- internet-radio/src/index.js
+++ internet-radio/src/index.js
@@ -30,9 +30,9 @@ const RadioHeader = () => {
 
 function RadioContainer({genres}) {
     return (
-        <div>
+        <div className="card-body row justify-content-around align-items-center">
             {genres.map(genre => (
-                <RadioButton key={genre.id} genre={genre.genre}/>
+                <RadioButton key={genre.id} genre={genre}/>
             ))}
         </div>
     );
@@ -42,10 +42,10 @@ RadioContainer.prototypes = {
     genres: PropTypes.array.isRequired
 };
 
-function RadioButton({key, genre}) {
+function RadioButton({genre}) {
     return (
-        <a className={key > 10 ? "btn btn-primary btn-md" : key > 20 ? "btn btn-primary btn-sm" : key > 30 ? "btn btn-primary btn-xs" : "btn btn-primary btn-lg"} href="#">
-            {genre}
+        <a className={genre.id > 30 ? "btn btn-light btn-sm mx-2 my-1" : genre.id > 15 ? "btn btn-light btn-md mx-2 my-1" : "btn btn-light btn-lg mx-2 my-1"} href="#">
+            {genre.genre}
         </a>
     );
 };
@@ -71,6 +71,150 @@ var radioDatas = [
         id: 4,
         genre: "Latino"
     },
+    {
+        id: 5,
+        genre: "Jazz"
+    },
+    {
+        id: 6,
+        genre: "Blues"
+    },
+    {
+        id: 7,
+        genre: "Rock"
+    },
+    {
+        id: 8,
+        genre: "Top 50"
+    },
+    {
+        id: 9,
+        genre: "Indie"
+    },
+    {
+        id: 10,
+        genre: "Electro"
+    },
+    {
+        id: 11,
+        genre: "Metal"
+    },
+    {
+        id: 12,
+        genre: "Variete"
+    },
+    {
+        id: 13,
+        genre: "Traditionnel"
+    },
+    {
+        id: 14,
+        genre: "World Music"
+    },
+    {
+        id: 15,
+        genre: "Psychedelics"
+    },
+    {
+        id: 16,
+        genre: "Transe"
+    },
+    {
+        id: 17,
+        genre: "Raggamuffin"
+    },
+    {
+        id: 18,
+        genre: "Dancehall"
+    },
+    {
+        id: 19,
+        genre: "Zouk"
+    },
+    {
+        id: 20,
+        genre: "Salsa"
+    },
+    {
+        id: 21,
+        genre: "Batchata"
+    },
+    {
+        id: 22,
+        genre: "Cumbia"
+    },
+    {
+        id: 23,
+        genre: "Kizomba"
+    },
+    {
+        id: 24,
+        genre: "Swing"
+    },
+    {
+        id: 25,
+        genre: "Zouk"
+    },
+    {
+        id: 26,
+        genre: "Salsa"
+    },
+    {
+        id: 27,
+        genre: "Batchata"
+    },
+    {
+        id: 28,
+        genre: "Cumbia"
+    },
+    {
+        id: 29,
+        genre: "Kizomba"
+    },
+    {
+        id: 30,
+        genre: "Swing"
+    },
+    {
+        id: 31,
+        genre: "Jungle"
+    },
+    {
+        id: 32,
+        genre: "SoudTracks"
+    },
+    {
+        id: 33,
+        genre: "RnB"
+    },
+    {
+        id: 34,
+        genre: "Soul"
+    },
+    {
+        id: 35,
+        genre: "Folk"
+    },
+    {
+        id: 36,
+        genre: "Country"
+    },
+    {
+        id: 37,
+        genre: "Classic"
+    },
+    {
+        id: 38,
+        genre: "Grunge"
+    },
+    {
+        id: 39,
+        genre: "Disco"
+    },
+    {
+        id: 40,
+        genre: "Chill"
+    }
 ];
 
 ReactDOM.render(<RadioComponent genres={radioDatas}/>, document.getElementById("root"));
