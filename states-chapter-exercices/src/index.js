ef47ca5c802ebeca16060956295066e02a61c50a
Anh Pati <romain-felicianne@hotmail.fr>
2019-04-22 15:34:50 +0200

    Mise en place du projet States Chapter Exercices

---
+++ states-chapter-exercices/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from 'prop-types';
+import "./index.css";
+
+const ExercicesContainer = () => {
+    return (
+        <div className="mainContainer">
+        
+        </div>
+    )
+}
+
+ReactDOM.render(<ExercicesContainer/>, document.getElementById("root"));
