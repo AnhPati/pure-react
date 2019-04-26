e443f545713ffbf6004fefd5eab58d8075df7eb0
Anh Pati <romain-felicianne@hotmail.fr>
2019-04-26 18:23:59 +0200

    Création des différents composants du chapitre. Example : Entrée controlée TrickInput : Entrée impossible NoNumbersInput : Entrée controlée qui n'accepte pas les chiffres EasyInput : Entrée no controlée simple RefInput : Entrée simple controlée par l'attribut "ref" RefInputStateLess : Entrée simple controlée sans état

--- input-controls-chapter/src/index.js
+++ input-controls-chapter/src/index.js
@@ -1,12 +1,128 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const MainContainer = () => {
+    return (
+        <div>
+            <h1>WESH</h1>
+            <Example/>
+            <TrickInput/>
+            <NoNumbersInput/>
+            <EasyInput/>
+            <RefInput/>
+            <RefInputStateless/>
+        </div>
+    );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Example extends React.Component {
+    state = { text: '' };
+
+    handleChange = (event) => {
+        this.setState({
+            text: event.target.value
+        });
+    };
+
+    render() {
+        return (
+            <div>
+            <input type="text"
+                value={this.state.text}
+                onChange={this.handleChange}
+            />
+            {console.log(this.state.text)}
+            </div>
+        );
+    };
+};
+
+class TrickInput extends React.Component {
+    state = {
+        text: 'try typing something'
+    };
+
+    handleChage = (event) => {
+        this.setState({
+            text: 'haha nope'
+        });
+    };
+
+    render() {
+        return (
+            <input type="text"
+                value={this.state.text}
+                onChange={this.handleChange}
+            />
+        );
+    };
+};
+
+class NoNumbersInput extends React.Component {
+    state = {
+        text: ''
+    };
+
+    handleChange = (event) => {
+        let text = event.target.value;
+        text = text.replace(/[0-9]/g, '');
+        this.setState({ text });
+    };
+
+    render() {
+        return (
+            <input type="text"
+                value={this.state.text}
+                onChange={this.handleChange}
+            />
+        );
+    };
+};
+
+const EasyInput = () => (
+    <input type="text"/>
+);
+
+class RefInput extends React.Component {
+    showValue = () => {
+        alert(`Input contains : ${this.input.value}`);
+    };
+
+    render() {
+        return (
+            <div>
+                <input
+                    type="text"
+                    ref={input => this.input = input}
+                />
+                <button onClick={this.showValue}>
+                    Alert the Value !
+                </button>
+            </div>
+        );
+    };
+};
+
+const RefInputStateless = () => {
+    let input;
+
+    const showValue = () => {
+        alert(`Input contains: ${input.value}`);
+    };
+
+    return (
+        <div>
+            <input
+                type="text"
+                ref={i => input = i}
+            />
+            <button onClick={showValue}>
+                Alert the Value !
+            </button>
+        </div>
+    );
+};
+
+ReactDOM.render(<MainContainer/>, document.getElementById('root'));
