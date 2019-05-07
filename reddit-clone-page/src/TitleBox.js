2f48df0bbcdbce5d2d153000e502a7765df238f1
Anh Pati <romain-felicianne@hotmail.fr>
2019-05-07 16:02:48 +0200

    Définitions des styles globaux de TitleBox avec Bootstrap. Ajout de styles dans TitleBox.css.

--- reddit-clone-page/src/TitleBox.js
+++ reddit-clone-page/src/TitleBox.js
@@ -5,7 +5,10 @@ import "./TitleBox.css";
 function TitleBox({post}) {
     return (
         <div className="d-flex w-100">
-            <a href={post.url}><h1 className="">{post.title}</h1></a><a href="#"><span>{post.subreddit_name_prefixed}</span></a>
+            <h3 className="titlePost">
+                <a href={post.url} className="cLink-none">{post.title}</a>
+                <span className="titleSource mx-2"><a href="#" className="cLink-none">{post.subreddit_name_prefixed}</a></span>
+            </h3>
         </div>
     )
 }
